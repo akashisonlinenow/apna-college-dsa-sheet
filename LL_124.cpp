e9e443fc65785ad51088142d96586b3b97c61ba5
Akash Shridharan <akashshridharan2802@gmail.com>
2023-02-09 23:25:31 +0530

    Add files via upload

---
+++ LL_124.cpp
@@ -0,0 +1,41 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution
+{
+public:
+    ListNode *reverseList(ListNode *head)
+    {
+        // ListNode* prevptr = NULL;          Iterative method...
+        // ListNode* currptr = head;
+        // ListNode* nextptr;
+
+        // while(currptr!=NULL){
+        //     nextptr = currptr->next;
+        //     currptr->next = prevptr;
+
+        //     prevptr = currptr;
+        //     currptr = nextptr;
+
+        // }
+        // return prevptr;
+
+        if (head == NULL || head->next == NULL)
+        { // recurssive method...
+            return head;
+        }
+
+        ListNode *newhead = reverseList(head->next);
+        head->next->next = head;
+        head->next = NULL;
+
+        return newhead;
+    }
+};
