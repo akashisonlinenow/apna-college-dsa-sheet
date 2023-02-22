4483a43e4a75ae77c2f9898839a9c7978caa5a54
Akash Shridharan <akashshridharan2802@gmail.com>
2023-02-22 23:20:24 +0530

    Add files via upload

---
+++ SQ_165.cpp
@@ -0,0 +1,78 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+class SortedStack{
+public:
+	stack<int> s;
+	void sort();
+};
+
+void printStack(stack<int> s)
+{
+    while (!s.empty())
+    {
+        printf("%d ", s.top());
+       	s.pop();
+    }
+    printf("\n");
+}
+
+int main()
+{
+int t;
+cin>>t;
+while(t--)
+{
+	SortedStack *ss = new SortedStack();
+	int n;
+	cin>>n;
+	for(int i=0;i<n;i++)
+	{
+	int k;
+	cin>>k;
+	ss->s.push(k);
+	}
+	ss->sort();
+	printStack(ss->s);
+}
+}
+// } Driver Code Ends
+
+
+/*The structure of the class is
+class SortedStack{
+public:
+	stack<int> s;
+	void sort();
+};
+*/
+
+void insert(stack<int> &s, int temp){
+    if(s.size()==0 || s.top()==temp){
+        s.push(temp);
+        return;
+    }
+    int val = s.top();
+    s.pop();
+    insert(s,temp);
+    s.push(val);
+    return;
+}
+
+/* The below method sorts the stack s 
+you are required to complete the below method */
+void SortedStack :: sort(int x)
+{
+    if(s.size()==0)      // sorting a stack with recurssion refer to aditya verma utube
+    return;
+    
+    int temp = s.top();
+    s.pop();
+    sort();
+    insert(s,temp);
+    return;
+    
+    
+   //Your code here
+}
